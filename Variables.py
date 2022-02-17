db3ae4a7b5d18392ac6e9e3b89cac268dc40dd1b
EduardMoiseenko <Prorok87@gmail.com>
2022-02-17 17:02:18 +0200

    Изучил урок по переменным

--- Variables.py
+++ Variables.py
@@ -2,3 +2,28 @@ x = 2
 y = 3
 z = x + y
 print('z равняется ' + str(z))
+print('----------------------------')
+number = 3
+number = 4
+number2 = 5
+result = number + number2
+print(result)
+print('----------------------------')
+num1 = num2 = 5
+print(num1, num2)
+print('----------------------------')
+num_1, num_2 = 5, 7
+print(num_1, num_2)
+print('----------------------------')
+swap1 = 8
+swap2 = 9
+swap1, swap2 = swap2, swap1 + swap2
+print(swap1, swap2)
+print('----------------------------')
+swap2 -= number
+print(swap2)
+print('----------------------------')
+z, x, *c = [1, 2, 3, 4, 5, 6]
+print(z)
+print(x)
+print(c)
